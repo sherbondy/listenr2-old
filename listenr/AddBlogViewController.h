22b7bf7edf45ac2f257bef0efd266efda8888dd7
Ethan Sherbondy <sherbondye@gmail.com>
2012-08-12 16:11:56 -0700

    addblogvc in separate file now.

---
+++ listenr/AddBlogViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBlogViewController.h
+//  listenr
+//
+//  Created by Ethan Sherbondy on 8/12/12.
+//  Copyright (c) 2012 Unidextrous. All rights reserved.
+//
+
+#import "JMStaticContentTableViewController.h"
+
+@interface AddBlogViewController : JMStaticContentTableViewController <UITextFieldDelegate>
+
+@end
